d3c5bf77177156709a199dfe625b1ee27ce15382
TheAlchemist0033 <36774748+TheAlchemist0033@users.noreply.github.com>
2022-12-21 17:19:28 -0700

    Create bump.js

---
+++ maindb/bump.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const bumpSchema = new mongoose.Schema({
+    serverID: {
+      type: String,
+      required: true
+    },
+    bumpTime: {
+      type: Date,
+      required: true
+    }
+  });
+const Bump = mongoose.model('Bump', bumpSchema);
+module.exports = Bump;
