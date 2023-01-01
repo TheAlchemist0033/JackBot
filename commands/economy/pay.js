2977f40febcbf633b0ba5f84410a059110a3df93
TheAlchemist0033 <chandler.lyon@student.nmt.edu>
2023-01-01 15:12:06 -0700

    new file:   commands/economy/pay.js 	modified:   commands/economy/rob.js

---
+++ commands/economy/pay.js
@@ -0,0 +1,97 @@
+const Discord = require('discord.js');
+const mongoose = require('mongoose');
+
+module.exports = {
+    name: 'pay',
+    description: 'Pay a user a specified amount.',
+    usage: '+pay @user <amount>',
+    async execute(client, message, args) {
+        // Set up a Mongoose model for the user balances
+        const Balance = require('../../maindb/bal.js');
+
+        // Check if the user has mentioned another user to pay
+        if (!message.mentions.users.size) {
+            return message.reply('You need to mention a user to pay them!');
+        }
+
+        // Check if the user has provided an amount to pay
+        if (!args[1]) {
+            return message.reply('You need to provide an amount to pay!');
+        }
+
+        // Get the user to pay
+        const userToPay = message.mentions.users.first();
+
+        // Check if the user trying to pay is the same as the user being paid
+        if (userToPay.id === message.author.id) {
+            return message.channel.send("You can't pay yourself!");
+        }
+
+        // Find the user's balance in the database
+        Balance.findOne({
+            userID: message.author.id,
+            serverID: message.guildId
+        }, async (err, res) => {
+            if (err) return console.log(err);
+
+            // If the user doesn't have a balance, create one with a starting balance of 0
+            if (!res) {
+                ball = new Balance({
+                    userID: message.author.id,
+                    serverID: message.guildId,
+                    balance: 100,
+                    username: `${message.author.username}#${message.author.discriminator}`,
+                    inventory: {},
+                    cooldown: 0,
+                    fishcool: 0,
+                    workcooldown: 0,
+                    robcool: 0
+                });
+                await ball.save().catch((err) => console.log(err));
+                return message.channel.send("You don't have a balance to pay from!");
+            }
+
+            // Check if the user has enough money to pay the specified amount
+            if (res.balance < args[1]) {
+                return message.channel.send("You don't have enough money to pay that amount!");
+            }
+
+            // Find the user to pay's balance in the database
+            Balance.findOne({
+                userID: userToPay.id
+            }, async (err, rres) => {
+                if (err) return console.log(err);
+
+                // If the user being paid doesn't have a balance, create one with a starting balance of 0
+                if (!rres) {
+                    ball = new Balance({
+                        userID: userToPay.id,
+                        serverID: message.guildId,
+                        balance: 100,
+                        username: `${userToPay.username}#${userToPay.discriminator}`,
+                        inventory: {},
+                        cooldown: 0,
+                        fishcool: 0,
+                        workcooldown: 0,
+                        robcool: 0
+                    });
+                    await ball.save().catch((err) => console
+                    .log(err));
+                }
+
+                // Update the balances of both users
+                await Balance.findOneAndUpdate(
+                    { userID: message.author.id },
+                    { $inc: { balance: -args[1] } }
+                );
+                await Balance.findOneAndUpdate(
+                    { userID: userToPay.id },
+                    { $inc: { balance: args[1] } }
+                );
+
+                // Send a message with the updated balances
+                message.channel.send(`${message.author.username} has paid ${userToPay.username} ${args[1]} coins.`);
+            });
+        });
+    }
+};
--- commands/economy/rob.js
+++ commands/economy/rob.js
@@ -2,99 +2,120 @@ const Discord = require('discord.js');
 const mongoose = require('mongoose');
 
 module.exports = {
-  name: 'rob',
-  description: 'Try to rob another user.',
-  usage: '+rob @user',
-  async execute(client, message, args) {
-    // Set up a Mongoose model for the user balances
-    const Balance = require('../../maindb/bal.js');
+    name: 'rob',
+    description: 'Try to rob another user.',
+    usage: '+rob @user',
+    async execute(client, message, args) {
+        // Set up a Mongoose model for the user balances
+        const Balance = require('../../maindb/bal.js');
 
-    // Check if the user has mentioned another user to rob
-    if (!message.mentions.users.size) {
-      return message.reply('You need to mention a user to rob them!');
-    }
-
-    // Get the user to rob
-    const userToRob = message.mentions.users.first();
-
-    // Check if the user trying to rob is the same as the user being robbed
-    if (userToRob.id === message.author.id) {
-      return message.channel.send("You can't rob yourself!");
-    }
-
-    // Find the user's balance in the database
-    Balance.findOne({ userID: message.author.id,serverID:message.guildId}, async (err, res) => {
-      if (err) return console.log(err);
-
-      // If the user doesn't have a balance, create one with a starting balance of 0
-      if (!res) {
-        ball = new Balance({
-          uuserID: userToRob.id,
-          serverID:message.guildId,
-          balance:100,
-          username: `${message.author.username}#${message.author.discriminator}`,
-          inventory:{},
-          cooldown:0,
-          fishcool:0,
-          workcooldown:0,
-          robcool:0
-        });
-        await ball.save().catch((err) => console.log(err));
-      }else{
-
-      // Find the user to rob's balance in the database
-      Balance.findOne({ userID: userToRob.id }, async (err, rres) => {
-        if (err) return console.log(err);
-
-        // If the user being robbed doesn't have a balance, create one with a starting balance of 0
-        if (!rres) {
-          ball = new Balance({
-/*  userID: String,
-    serverID:String,
-    balance: Number,
-    username:String,
-    inventory:{type:mongoose.Schema.Types.Mixed},
-    cooldown:Number,
-    fishcool:Number,
-    workcooldown:Number,
-    robcool:Number*/
-            userID: userToRob.id,
-            serverID:message.guildId,
-            balance:100,
-            username: `${userToRob.username}#${userToRob.discriminator}`,
-            inventory:{},
-            cooldown:0,
-            fishcool:0,
-            workcooldown:0,
-            robcool:0
-          });
-          await ball.save().catch((err) => console.log(err));
-          message.cannel.send("You couldn't rob that user! They do not have any money!")
-        }else{
-
-        // Generate a random number between 0 and the user being robbed's balance
-        if(rres.balance <= 50){
-            return message.channel.send("That person is too poor to rob.");
-        }
-        const amount = Math.floor(Math.random() * rres.balance);
-
-        // Update the balances of both users
-        await Balance.findOneAndUpdate(
-          { userID: message.author.id },
-          { $inc: { balance: amount } },{$set:{robcool:new Date().getTime() + 30000}}
-        );
-        await Balance.findOneAndUpdate(
-          { userID: userToRob.id },
-          { $inc: { balance: -amount } }
-        );
-
-        // Send a message to the channel
-        message.channel.send(
-          `${message.author.username} robbed ${amount} Zhmorgles (ZML) from ${userToRob.username}!`); 
+        // Check if the user has mentioned another user to rob
+        if (!message.mentions.users.size) {
+            return message.reply('You need to mention a user to rob them!');
         }
 
+        // Get the user to rob
+        const userToRob = message.mentions.users.first();
+
+        // Check if the user trying to rob is the same as the user being robbed
+        if (userToRob.id === message.author.id) {
+            return message.channel.send("You can't rob yourself!");
+        }
+
+        // Find the user's balance in the database
+        Balance.findOne({
+            userID: message.author.id,
+            serverID: message.guildId
+        }, async (err, res) => {
+            if (err) return console.log(err);
+            // If the user doesn't have a balance, create one with a starting balance of 0
+            if (!res) {
+                ball = new Balance({
+                    uuserID: userToRob.id,
+                    serverID: message.guildId,
+                    balance: 100,
+                    username: `${message.author.username}#${message.author.discriminator}`,
+                    inventory: {},
+                    cooldown: 0,
+                    fishcool: 0,
+                    workcooldown: 0,
+                    robcool: 0
+                });
+                await ball.save().catch((err) => console.log(err));
+            } else {
+                if (res.robcool) {
+                    if (res.robcool < new Date().getTime()) {
+
+                        // Find the user to rob's balance in the database
+                        Balance.findOne({
+                            userID: userToRob.id
+                        }, async (err, rres) => {
+                            if (err) return console.log(err);
+
+                            // If the user being robbed doesn't have a balance, create one with a starting balance of 0
+                            if (!rres) {
+                                ball = new Balance({
+                                    /*  userID: String,
+                                        serverID:String,
+                                        balance: Number,
+                                        username:String,
+                                        inventory:{type:mongoose.Schema.Types.Mixed},
+                                        cooldown:Number,
+                                        fishcool:Number,
+                                        workcooldown:Number,
+                                        robcool:Number*/
+                                    userID: userToRob.id,
+                                    serverID: message.guildId,
+                                    balance: 100,
+                                    username: `${userToRob.username}#${userToRob.discriminator}`,
+                                    inventory: {},
+                                    cooldown: 0,
+                                    fishcool: 0,
+                                    workcooldown: 0,
+                                    robcool: 0
+                                });
+                                await ball.save().catch((err) => console.log(err));
+                                message.cannel.send("You couldn't rob that user! They do not have any money!")
+                            } else {
+
+                                // Generate a random number between 0 and the user being robbed's balance
+                                if (rres.balance <= 50) {
+                                    return message.channel.send("That person is too poor to rob.");
+                                }
+                                const amount = Math.floor(Math.random() * rres.balance);
+
+                                // Update the balances of both users
+                                await Balance.findOneAndUpdate({
+                                    userID: message.author.id,
+                                    serverID: message.guildId
+                                }, {
+                                    $inc: {
+                                        balance: amount
+                                    },
+                                    $set: {
+                                        robcool: new Date().getTime() + 30000
+                                    }
+                                });
+
+                                await Balance.findOneAndUpdate({
+                                    userID: userToRob.id
+                                }, {
+                                    $inc: {
+                                        balance: -amount
+                                    }
+                                });
+
+                                // Send a message to the channel
+                                message.channel.send(
+                                    `${message.author.username} robbed ${amount} Zhmorgles (ZML) from ${userToRob.username}!`);
+                            }
+
+                        });
+                    }
+                } else {
+                    message.channel.send("You need to wait at least 30 seconds before robbing another user!")
+                }
+            }
         });
-    }
-    });
-  },
+    },
 };
