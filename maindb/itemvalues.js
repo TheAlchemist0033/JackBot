0305516c5b950e45e14a2b60569856043c043a9d
TheAlchemist0033 <chandler.lyon@student.nmt.edu>
2023-01-01 00:12:16 -0700

    modified:   maindb/itemvalues.js

--- maindb/itemvalues.js
+++ maindb/itemvalues.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const itemSchema = new mongoose.Schema({
-    exists:1,
+    exists:Number,
     items:{type:mongoose.Schema.Types.Mixed}
   });
   const items = mongoose.model('items', itemSchema);
