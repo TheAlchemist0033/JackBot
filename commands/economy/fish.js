6e666e5434703512a167aa348d15342f258f2668
TheAlchemist0033 <chandler.lyon@student.nmt.edu>
2023-01-01 02:17:02 -0700

    modified:   commands/economy/fish.js 	modified:   commands/economy/work.js

--- commands/economy/fish.js
+++ commands/economy/fish.js
@@ -28,7 +28,8 @@ module.exports = {
         if(res.fishcool){
             console.log(res.fishcool - new Date().getTime())
             if(!(res.fishcool > new Date().getTime()) ){
-                return message.channel.send("You need to wait 3 minutes between teleportation attempts.")
+                return message.channel.send(`You need to wait 3 minutes between teleportation attempts. Time remaining: ${(res.cooldown - new Date().getTime()) /
+                1000} seconds`);
             }
         }
 
