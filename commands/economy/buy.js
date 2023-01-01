04c9c1cbd8543c66c89a5147f179c6736f3ed910
TheAlchemist0033 <chandler.lyon@student.nmt.edu>
2023-01-01 01:48:38 -0700

    modified:   commands/economy/buy.js

--- commands/economy/buy.js
+++ commands/economy/buy.js
@@ -11,72 +11,70 @@ module.exports = {
     const Balance = require("../../maindb/bal.js");
     const Shop = require("../../maindb/shop.js");
 
-    // Initialize the balance model
-    const balance = new Balance({ userID: message.author.id, serverID: message.guildId });
-
     // Check if the user has specified an item to purchase
     if (!args.length) {
       return message.channel.send(`You didn't specify an item to purchase!`);
     }
     const item = args[0].toLowerCase();
+
     // Check if the item is available for purchase
-    Shop.findOne({exists:1},async (err,res)=>{
+    shop.findOne({exists:1},async (err,res)=>{
 
-      if(err){
-          message.channel.send("I've encountered an error when searching for the shop inventory.");
-          return console.log(err);
-      }else if(!res){
-          return message.channel.send("Shop has not been initialized by bot owner.");
-      }else{
-          const availableItems = Object.keys(res.stock);
+        if(err){
+            message.channel.send("I've encountered an error when searching for the shop inventory.");
+            return console.log(err);
+        }else if(!res){
+            return message.channel.send("Shop has not been initialized by bot owner.");
+        }else{
+            const availableItems = Object.keys(res.stock);
+ 
+    //const availableItems = ['glipglop', 'smoglethorp', 'bleemerbloogle','glip','smog','blee'];
+    if (!availableItems.includes(item)) {
+      return message.channel.send(`${item} is not available for purchase.`);
+    }
 
-  //const availableItems = ['glipglop', 'smoglethorp', 'bleemerbloogle','glip','smog','blee'];
-  if (!availableItems.includes(item)) {
-    return message.channel.send(`${item} is not available for purchase.`);
-  }
+    // Check the cost of the item
+    var cost = res.stock[item].cost;
+    // Find the user's balance in the database
+    let balance = await Balance.findOne({ userID: message.author.id ,serverID:message.guildId});
 
-  // Check the cost of the item
-  var cost = res.stock[item].cost;
-  // Find the user's balance in the database
-  balance = await Balance.findOne({ userID: message.author.id ,serverID:message.guildId});
-
-  // If the user doesn't have a balance, create one with a starting balance of 100
-  if (!balance) {
-    balance = new Balance({
-      userID: message.author.id,
-      serverID:message.guildId,
-      username:message.author.username+"#"+message.author.discriminator,
-      balance: 100,
-      inventory:{},
-      cooldown:new Date().getTime()
-    });
-    await balance.save();
+    // If the user doesn't have a balance, create one with a starting balance of 100
+    if (!balance) {
+      balance = new Balance({
+        userID: message.author.id,
+        serverID:message.guildId,
+        username:message.author.username+"#"+message.author.discriminator,
+        balance: 100,
+        inventory:{},
+        cooldown:new Date().getTime()
+      });
+      await balance.save();
     return message.channel.send("You did not have an account yet, please try running this command again. You currently have 100 Zhmorgles (ZML).");
+    }
+
+    // Check if the user has enough money to make the purchase
+    if (balance.balance < cost) {
+      return message.channel.send(`You don't have enough Zhmorgles to purchase ${item}!`);
+    }
+
+    // Subtract the cost of the item from the user's balance
+    balance.balance -= cost;
+
+    if(balance.inventory[item]){
+        balance.inventory[item] += 1;
+       // balance.save().catch(err=>console.log(err));
+    }else{
+      //whatever you do dont go look at my commit history to see how stupid i was when i was working on this...
+         balance.inventory[item] = 1;
+    }
+    await balance.save().catch(err=>console.log(err));
+    // Send a message confirming the purchase
+    message.channel.send(`You have purchased ${item} for ${cost} Zhmorgles. Your balance is now ${balance.balance} ZML.`);
+
+
+    }
+})
   }
-
-  // Check if the user has enough money to make the purchase
-  if (balance.balance < cost) {
-    return message.channel.send(`You don't have enough Zhmorgles to purchase ${item}!`);
-  }
-
-  // Subtract the cost of the item from the user's balance
-  balance.balance -= cost;
-
-  // Update the inventory with the new item
-  if(balance.inventory[item]){
-      balance.inventory[item] += 1;
-  }else{
-    //whatever you do dont go look at my commit history to see how stupid i was when i was working on this...
-       balance.inventory[item] = 1;
-  }
-
-  // Save the updated balance
-  await balance.save().catch(err=>console.log(err));
-
-  // Send a message confirming the purchase
-  message.channel.send(`You have purchased ${item} for ${cost} Zhmorgles. Your balance is now ${balance.balance} ZML.`);
-  }
-});
-  
-  },
 };
+
+   
