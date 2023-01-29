63ef7851345c33111906a0c9ea1713d49df8e983
TheAlchemist0033 <chandler.lyon@student.nmt.edu>
2023-01-28 18:23:47 -0700

    modified:   commands/utilities/debug.js

--- commands/utilities/debug.js
+++ commands/utilities/debug.js
@@ -6,21 +6,21 @@ module.exports = {
     description: 'For debugging use.',
     usage: '+debug [data]',
     async execute(client, message, args) {
-      var delM = await message.channel.send("10");
-      var cDown = 10
-      async function mss(){
-        if(cDown > 0){
-          setTimeout(async () => {
-            cD = (cDown-1).toString() 
-            delM = await delM.edit(cD)
-            mss()
-          },1000)
-        }else{
-          delM.delete()
-        }
-          
+      var counter = 10;
+      var cmess = counter.toString();
+      mss = await message.channel.send(cmess);
+      function recurse(){
+        if(counter >= 0){
+        setTimeout(async()=>{
+          counter -= 1 
+          cmess = counter.toString()
+          mss = await mss.edit(cmess);
+          recurse();
+        },1000)
+       
       }
-      mss()
 
+      }
+      recurse()
     },
 };
